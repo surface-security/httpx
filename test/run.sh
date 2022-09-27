6929f75653571d7421e944bca8ca58adf1f2a2d1
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2022-09-27 20:52:36 +0300

    Fix make tests for

--- test/run.sh
+++ test/run.sh
@@ -6,13 +6,15 @@ cd $(dirname $0)
 cd ..
 
 NAME=${1}
+shift
 
+echo "$NAME"
 rm -fr test/output
 
 docker run --rm \
-           -v $(pwd)/test/input:/input:ro \
-           -v $(pwd)/test/output:/output \
-           ${NAME} \
+           -v "$(pwd)/test/input:/input:ro" \
+           -v "$(pwd)/test/output:/output" \
+           "${NAME}" \
            "$@" \
            /input/input.txt
 
